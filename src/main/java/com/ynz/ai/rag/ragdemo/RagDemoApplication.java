448d8f6700c326418cf13799cf63f71fb5f8806b
Yichun Zhao <zhaoyichun@hotmail.com>
2026-03-14 14:26:46 +0100

    Initial commit

---
+++ src/main/java/com/ynz/ai/rag/ragdemo/RagDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.ai.rag.ragdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynz/ai/rag/ragdemo/RagDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.ai.rag.ragdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
