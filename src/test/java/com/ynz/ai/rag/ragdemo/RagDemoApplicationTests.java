4f89673f40c1288644a2bf0dd75b0d47707c28c2
Yichun Zhao <zhaoyichun@hotmail.com>
2026-06-03 01:22:19 +0200

    Align Spring AI and Boot versions

--- src/test/java/com/ynz/ai/rag/ragdemo/RagDemoApplicationTests.java
+++ src/test/java/com/ynz/ai/rag/ragdemo/RagDemoApplicationTests.java
@@ -1,10 +1,19 @@
 package com.ynz.ai.rag.ragdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(properties = "springdoc.swagger-ui.path=/swagger-ui.html")
 class RagDemoApplicationTests {
+
+	@MockitoBean
+	private ChatModel chatModel;
+
+	@MockitoBean
+	private VectorStore vectorStore;
 
 	@Test
 	void contextLoads() {
